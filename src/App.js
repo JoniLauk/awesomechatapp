94704e8d2ac74b9f7a03a2df65a5c67b09d98b15
Joni Laukkanen <jonilaukkanen1@gmail.com>
2021-09-29 14:30:12 +0300

    heroku test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <form>
+        <label>
+          Username
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Password
+          <input type="password" name="password" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
