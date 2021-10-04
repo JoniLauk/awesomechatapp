cc8ff7ec245b241a409452ea47695e0a3fe2fe30
Matias Vainio <matias.s.vainio@gmail.com>
2021-10-04 22:35:29 +0300

    added some comments

--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -1,5 +1,12 @@
 const jwt = require('jsonwebtoken');
 
+/**
+ * Extracts JsonWebToken from the request body and adds it to
+ * request objects token field.
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
 const tokenExtractor = (req, res, next) => {
   const auth = req.body('authorization');
   if (auth && auth.toLowerCase().startsWith('bearer ')) {
@@ -8,6 +15,12 @@ const tokenExtractor = (req, res, next) => {
   next();
 };
 
+/**
+ * Verifies extracted token with the secret stored on the server.
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
 const authenticator = (req, res, next) => {
   if (req.token) {
     jwt.verify(req.token, process.env.SECRET, (err, user) => {
@@ -16,8 +29,8 @@ const authenticator = (req, res, next) => {
           error: 'Token missing',
         });
       }
+      req.user = user;
     });
-    req.user = user;
     next();
   } else {
     next();
