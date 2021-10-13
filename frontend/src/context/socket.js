66169af02112d0d6dd90ade1875776bfc4052ee3
Joni Laukkanen <jonilaukkanen1@gmail.com>
2021-10-13 22:56:05 +0300

    commit 7 for heroku with socket

--- frontend/src/context/socket.js
+++ frontend/src/context/socket.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import io from 'socket.io-client';
 
-export const socket = io.connect(
-  'https://awesomechatappbeta.herokuapp.com/login'
-);
+export const socket = io.connect('https://awesomechatappbeta.herokuapp.com/');
 export const SocketContext = React.createContext();
